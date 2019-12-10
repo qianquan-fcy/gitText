9b5dfd02f9777d74d85de8d87caa9f80acd5c2af
qianquan <2897068188@qq.com>
2019-12-10 10:22:19 +0800

    woaini

---
+++ src/main/java/com/buba/hospital_back/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.buba.hospital_back.controller;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Created by Administrator on 2019/12/10 10:21
+ * @Version 1.0
+ **/
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("sssssssss");
+    }
+}
